4336b7593062c11e974bb08d305db0a586f7a628
paulgo <spam@heropaul.com>
2023-10-14 11:20:15 -0400

    Adds a control for displaying the active page in a scrollable progression

---
+++ core/hats/DotsHat.ts
@@ -0,0 +1,64 @@
+
+namespace ScrollApp
+{
+	/** */
+	export class DotsHat
+	{
+		readonly head;
+		
+		/** */
+		constructor()
+		{
+			this.head = hot.div(
+				{
+					textAlign: "center",
+				},
+				hot.css(" > SPAN", {
+					display: "inline-block",
+					width: "10px",
+					height: "10px",
+					margin: "3px",
+					borderRadius: "100%",
+					backgroundColor: "rgba(128, 128, 128)",
+				}),
+				hot.css(" > SPAN." + highlightClass, {
+					backgroundColor: "hsl(205, 100%, 50%)",
+				})
+			);
+		}
+		
+		/** */
+		insert(count: number, at = this.head.childElementCount)
+		{
+			const spans: HTMLSpanElement[] = [];
+			
+			for (let i = -1; ++i < count;)
+				spans.push(hot.span());
+			
+			at = Math.max(0, at);
+			at = Math.min(this.head.childElementCount, at);
+			
+			if (at >= this.head.childElementCount)
+			{
+				this.head.append(...spans);
+			}
+			else
+			{
+				const elements = Array.from(this.head.children);
+				elements[at].before(...spans);
+			}
+		}
+		
+		/** */
+		highlight(index: number)
+		{
+			index = Math.max(0, index);
+			index = Math.min(this.head.childElementCount - 1, index);
+			const children = Array.from(this.head.children);
+			children.forEach(e => e.classList.remove(highlightClass));
+			children[index].classList.add(highlightClass);
+		}
+	}
+	
+	const highlightClass = "highlight";
+}
