257186cb008ae0b5e61a0807b84bf2faf74871ab
paulgo <spam@heropaul.com>
2023-11-09 10:27:13 -0500

    Update web feeds

--- core/FeedsWeb.ts
+++ core/FeedsWeb.ts
@@ -2,10 +2,11 @@
 namespace ScrollApp
 {
 	//@ts-ignore
-	if (!DEBUG) return;
+	if (!WEB) return;
 	
 	export const feedsForWeb = [
-		
+		"https://raw.githubusercontent.com/HTMLFeeds/Examples/main/raccoons/index.txt",
+		"https://raw.githubusercontent.com/HTMLFeeds/Examples/main/trees/index.txt",
 	];
 }
 
